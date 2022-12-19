473bb0936cbaa164974cca8208dfd3607054dd7c
Ismael Miguelez <i.miguelez@fmcproduction.com>
2022-12-19 16:12:56 +0100

    pwa

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,32 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        {/* <meta name="description" content="Description" /> */}
+        {/* <meta name="keywords" content="Keywords" />
+        <title>Next.js PWA Example</title>
+        <link rel="manifest" href="/manifest.json" />
+        <link
+          href="/icons/favicon-16x16.png"
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+        />
+        <link
+          href="/icons/favicon-32x32.png"
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+        />
+        <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+        <meta name="theme-color" content="#317EFB" /> */}
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
